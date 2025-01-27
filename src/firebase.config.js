f0105f3585601d9be8b9003596eb6d7d543360b4
rohitkumar-27 <rohitkmr528@gmail.com>
2025-01-28 00:47:55 +0530

    Updated Firebase config to use VITE environment variables

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGLPXnPcK9_ihxq1jDgmXGYT5dMcXwm-g",
-  authDomain: "job-portal-50a81.firebaseapp.com",
-  projectId: "job-portal-50a81",
-  storageBucket: "job-portal-50a81.firebasestorage.app",
-  messagingSenderId: "958288515279",
-  appId: "1:958288515279:web:2a8e8682003ad7e2cce7ba"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
